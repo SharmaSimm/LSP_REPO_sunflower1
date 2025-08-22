824cc9337e514d9afa61e1df8ec1f2d42158e49e
ab0197 <basnetanil0197@gmail.com>
2025-08-22 13:12:20 -0400

    Assignment 1: Hello World

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Name: Shikshya Sharma
+ * This project is the first LSP assignment 
+ */
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
